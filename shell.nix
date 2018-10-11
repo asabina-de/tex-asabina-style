04f1adbd1623433aa3c72b09a1b9fc78f139795b
David Asabina <vid@bina.me>
2018-10-11 18:10:54 +0200

    Simplify nix-shell setup

--- shell.nix
+++ shell.nix
@@ -1,4 +1,37 @@
-{ system ? builtins.currentSystem }:
+with import <nixpkgs> {};
 
-let pkgs = import <nixpkgs> {};
-in pkgs.callPackage ./env.nix {}
+let
+  fonts = makeFontsConf { fontDirectories = [ montserrat ]; };
+  style = callPackage ./default.nix {};
+in stdenv.mkDerivation rec {
+  name = "${style.name}-env";
+  version = "0.1.0";
+  buildInputs = [
+    gnumake
+    (texlive.combine {
+      inherit (texlive)
+      scheme-basic
+
+      collection-fontsrecommended
+      collection-langgerman
+      collection-latexrecommended
+      metafont
+      numprint
+      xetex
+      xstring
+      ;
+
+      style = {
+        pkgs = [ style ];
+      };
+    })
+  ];
+
+  FONTCONFIG_FILE = fonts;
+
+  shellHook = ''
+    # https://github.com/NixOS/nix/issues/1056
+    export TERMINFO=/run/current-system/sw/share/terminfo
+    real_TERM=$TERM; TERM=xterm; TERM=$real_TERM; unset real_TERM
+  '';
+}
